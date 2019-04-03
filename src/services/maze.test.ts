6be548e447b40b13a0458aec64d520fed230ea3e
Stefan Bird <stefan.bird@catch.com.au>
2019-04-04 09:35:15 +1100

    Add maze builder

---
+++ src/services/maze.test.ts
@@ -0,0 +1,41 @@
+import Maze, { Direction, Location, move } from "./maze";
+
+describe("Maze", () => {
+  it("Should generate maze with the correct size", () => {
+    expect(new Maze({ width: 5 }).width).toEqual(5);
+    expect(new Maze({ width: 5 }).height).toEqual(5);
+
+    expect(new Maze({ width: 4, height: 8 }).width).toEqual(4);
+    expect(new Maze({ width: 4, height: 8 }).height).toEqual(8);
+  });
+
+  it("Should be possible to reach every cell", () => {
+    const maze = new Maze({ width: 11 });
+    const visitedCells = new Set<string>();
+
+    traverse({ x: 0, y: 0 });
+    expect(visitedCells.size).toEqual(maze.width * maze.height);
+
+    function traverse(location: Location) {
+      if (visitedCells.has(getKey(location))) {
+        return;
+      } else if (!maze.contains(location)) {
+        throw new Error("Moved outside maze");
+      }
+
+      visitedCells.add(getKey(location));
+
+      [Direction.UP, Direction.LEFT, Direction.DOWN, Direction.RIGHT].forEach(
+        direction => {
+          if (!maze.hasWall(location, direction)) {
+            traverse(move(location, direction));
+          }
+        }
+      );
+    }
+
+    function getKey(location: Location): string {
+      return JSON.stringify(location);
+    }
+  });
+});
---
+++ src/services/maze.ts
@@ -0,0 +1,274 @@
+import * as seedrandom from "seedrandom";
+
+export interface Location {
+  x: number;
+  y: number;
+}
+
+export enum Direction {
+  UP = 1,
+  DOWN = 2,
+  LEFT = 4,
+  RIGHT = 8
+}
+
+/** A marker to say that no path runs through this cell. Cells only ever exist
+ * in this state during the building process */
+const EMPTY = 16;
+
+const directions: ReadonlyArray<Direction> = [
+  Direction.UP,
+  Direction.DOWN,
+  Direction.LEFT,
+  Direction.RIGHT
+];
+
+class Maze {
+  /** The width of the maze */
+  public readonly width: number;
+
+  /** The height of the maze */
+  public readonly height: number;
+
+  /** The cells of the maze */
+  public readonly cells: ReadonlyArray<ReadonlyArray<number>>;
+
+  // noinspection JSSuspiciousNameCombination
+  public constructor({
+    randomSeed,
+    width,
+    height = width,
+    pathMaxLength = width * 2
+  }: {
+    width: number;
+    height?: number;
+    pathMaxLength?: number;
+    randomSeed?: string;
+  }) {
+    this.width = width;
+    this.height = height;
+    this.cells = Maze.build(randomSeed, width, height, pathMaxLength);
+  }
+
+  /**
+   * Returns true if there is a wall at the given location and direction
+   */
+  public hasWall(location: Location, direction: Direction) {
+    const { x, y } = location;
+
+    return (
+      this.contains(location) && (this.cells[x][y] & direction) === direction
+    );
+  }
+
+  /**
+   * Returns true if the location exists within the maze
+   *
+   * @param location The location to check
+   */
+  public contains(location: Location) {
+    return Maze.contains(this.cells, location);
+  }
+
+  /**
+   * Builds a maze
+   *
+   * @param randomSeed The seed for the maze
+   * @param width The width of the maze
+   * @param height The height of the maze
+   * @param pathMaxLength The maximum length of any path
+   */
+  private static build(
+    randomSeed: string | undefined,
+    width: number,
+    height: number,
+    pathMaxLength: number
+  ): number[][] {
+    // Mark the location just outside the top left corner as a possible starting
+    // point
+    const pathStarts: Location[] = [{ x: -1, y: 0 }];
+    const cells = new Array(width);
+    const random = seedrandom.alea(randomSeed);
+
+    for (let x = 0; x < cells.length; x++) {
+      cells[x] = new Array(height).fill(
+        EMPTY | Direction.UP | Direction.DOWN | Direction.LEFT | Direction.RIGHT
+      );
+    }
+
+    while (pathStarts.length) {
+      Maze.addPath(random, cells, pathStarts, pathMaxLength);
+    }
+
+    // Rebuild the wall in the top left corner that our first path knocked down
+    cells[0][0] |= Direction.LEFT;
+
+    return cells;
+  }
+
+  /**
+   * Chooses a random starting point and attempts to extend a path.
+   */
+  private static addPath(
+    random: seedrandom.prng,
+    cells: number[][],
+    pathStarts: Location[],
+    pathMaxLength: number
+  ) {
+    const pathStartIdx = Math.floor(pathStarts.length * random());
+    let currentLocation = Maze.addStep(random, cells, pathStarts[pathStartIdx]);
+
+    if (currentLocation) {
+      // It was possible to extend a path from this location - keep going until
+      // we get blocked or we reach the maximum path length
+      for (
+        let pathLength = 1;
+        currentLocation && pathLength < pathMaxLength;
+        pathLength++
+      ) {
+        pathStarts.push(currentLocation);
+        currentLocation = Maze.addStep(random, cells, currentLocation);
+      }
+    } else {
+      // This starting location is boxed in; remove it so we don't try it again
+      pathStarts.splice(pathStartIdx, 1);
+    }
+  }
+
+  /**
+   * Adds a step to a path if possible
+   *
+   * @param random The PRNG to use
+   * @param cells The cells to add a step to
+   * @param location The location of the new step
+   * @return The new location, or null if there aren't any empty cells adjacent
+   * to location
+   */
+  private static addStep(
+    random: seedrandom.prng,
+    cells: number[][],
+    location: Location
+  ): Location | null {
+    for (let direction of Maze.randomize(random, directions)) {
+      const newLocation = move(location, direction);
+
+      if (
+        Maze.contains(cells, newLocation) &&
+        Maze.isEmpty(cells, newLocation)
+      ) {
+        const { x, y } = newLocation;
+
+        cells[x][y] &= ~EMPTY;
+
+        Maze.removeWall(cells, location, direction);
+        return newLocation;
+      }
+    }
+
+    return null;
+  }
+
+  /**
+   * Removes a wall between two cells
+   *
+   * @param cells The cells to remove the wall from
+   * @param location The location to remove the wall from
+   * @param direction The direction of the wall
+   */
+  private static removeWall(
+    cells: number[][],
+    location: Location,
+    direction: Direction
+  ) {
+    const { x, y } = location;
+    const neighbor = move(location, direction);
+    const { x: neighborX, y: neighborY } = neighbor;
+
+    if (Maze.contains(cells, location)) {
+      cells[x][y] &= ~direction;
+    }
+    if (Maze.contains(cells, neighbor)) {
+      cells[neighborX][neighborY] &= ~opposite(direction);
+    }
+  }
+
+  /**
+   * Returns true if the location exists within the cells
+   */
+  private static contains(
+    cells: ReadonlyArray<ReadonlyArray<number>>,
+    { x, y }: Location
+  ): boolean {
+    return x >= 0 && y >= 0 && x < cells.length && y < cells[0].length;
+  }
+
+  /**
+   * Returns true if the given cell doesn't have a path going through it
+   */
+  private static isEmpty(
+    cells: ReadonlyArray<ReadonlyArray<number>>,
+    { x, y }: Location
+  ): boolean {
+    return (cells[x][y] & EMPTY) === EMPTY;
+  }
+
+  /**
+   * Returns a new array with the elements of array in a random order
+   */
+  private static randomize<T>(
+    random: seedrandom.prng,
+    array: ReadonlyArray<T>
+  ): T[] {
+    const result = array.slice();
+
+    for (let i = 0; i < result.length; i++) {
+      const swapWith = Math.floor(random() * result.length);
+      const tmp = result[i];
+      result[i] = result[swapWith];
+      result[swapWith] = tmp;
+    }
+
+    return result;
+  }
+}
+
+/**
+ * Returns the location delta for moving one step in a given direction
+ *
+ * @param location
+ * @param direction
+ */
+export function move(location: Location, direction: Direction): Location {
+  switch (direction) {
+    case Direction.UP:
+      return { x: location.x, y: location.y - 1 };
+    case Direction.DOWN:
+      return { x: location.x, y: location.y + 1 };
+    case Direction.LEFT:
+      return { x: location.x - 1, y: location.y };
+    case Direction.RIGHT:
+      return { x: location.x + 1, y: location.y };
+  }
+
+  debugger;
+}
+
+/**
+ * Returns the direction that is the opposite from the given direction
+ *
+ * @param direction
+ */
+export function opposite(direction: Direction): Direction {
+  switch (direction) {
+    case Direction.UP:
+      return Direction.DOWN;
+    case Direction.DOWN:
+      return Direction.UP;
+    case Direction.LEFT:
+      return Direction.RIGHT;
+    case Direction.RIGHT:
+      return Direction.LEFT;
+  }
+}
+
+export default Maze;
